4937a6b1d346c96d59ac4a9b8b5d5598f0bd7dc3
Kanishka <you@example.com>
2019-11-25 10:15:17 +0530

    converted to dep

--- cmd/cart-service/main.go
+++ cmd/cart-service/main.go
@@ -2,53 +2,51 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/go-redis/redis/v7"
 )
 
 func main() {
 	fmt.Println("Hello from cart service.")
 }
 
-func ExampleNewClient() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+// func ExampleNewClient() {
+// 	client := redis.NewClient(&redis.Options{
+// 		Addr:     "localhost:6379",
+// 		Password: "", // no password set
+// 		DB:       0,  // use default DB
+// 	})
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-	// Output: PONG <nil>
-}
+// 	pong, err := client.Ping().Result()
+// 	fmt.Println(pong, err)
+// 	// Output: PONG <nil>
+// }
 
-func ExampleClient() {
+// func ExampleClient() {
 
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+// 	client := redis.NewClient(&redis.Options{
+// 		Addr:     "localhost:6379",
+// 		Password: "", // no password set
+// 		DB:       0,  // use default DB
+// 	})
 
-	err := client.Set("key", "value", 0).Err()
-	if err != nil {
-		panic(err)
-	}
+// 	err := client.Set("key", "value", 0).Err()
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	val, err := client.Get("key").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key", val)
+// 	val, err := client.Get("key").Result()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("key", val)
 
-	val2, err := client.Get("key2").Result()
-	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("key2", val2)
-	}
-	// Output: key value
-	// key2 does not exist
-}
+// 	val2, err := client.Get("key2").Result()
+// 	if err == redis.Nil {
+// 		fmt.Println("key2 does not exist")
+// 	} else if err != nil {
+// 		panic(err)
+// 	} else {
+// 		fmt.Println("key2", val2)
+// 	}
+// 	// Output: key value
+// 	// key2 does not exist
+// }
