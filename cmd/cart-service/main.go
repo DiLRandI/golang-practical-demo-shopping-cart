316aae2367fc607c6f2db5b7cea5d525dc1f3e7a
Kanishka <you@example.com>
2019-11-25 20:03:50 +0530

    Cart Service Changes

--- cmd/cart-service/main.go
+++ cmd/cart-service/main.go
@@ -1,54 +1,103 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
+	"strings"
 
 	"github.com/go-redis/redis"
+	"github.com/julienschmidt/httprouter"
+	log "github.com/sirupsen/logrus"
+	"github.com/womblebob/uuid"
 )
 
+var client *redis.Client
+var a uuid.UUID
+
 func main() {
-	fmt.Println("Hello from cart service.")
+	log.Infoln("Starting the Cart Service")
+	defer log.Warningln("Exiting Cart service")
+
+	httpRouting()
 }
 
-func ExampleNewClient() {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
+func httpRouting() {
+	log.Infoln("Starting the HTTP serving for Cart Servicing")
 
-	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
-	// Output: PONG <nil>
+	router := httprouter.New()
+	router.DELETE("/Clear", ClearCart)
+	router.POST("/AddItem/:ItemId", AddItem)
+	router.GET("/GetItems", GetCartItems)
+
+	log.Fatal(http.ListenAndServe(":8090", router))
 }
 
-func ExampleClient() {
-
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-
-	err := client.Set("key", "value", 0).Err()
-	if err != nil {
-		panic(err)
+//ClearCart Clear Cart Items
+func ClearCart(rw http.ResponseWriter, r *http.Request, parm httprouter.Params) {
+	InitRedisConnection()
+	flush := client.FlushAll()
+	if flush != nil {
+		log.Infoln(flush)
 	}
+}
 
-	val, err := client.Get("key").Result()
+//AddItem Add Item to the Cart
+func AddItem(rw http.ResponseWriter, r *http.Request, parm httprouter.Params) {
+	InitRedisConnection()
+	id := parm.ByName("ItemId")
+	cart, err := GetExistingCart()
 	if err != nil {
-		panic(err)
+		log.Errorln(err)
+		http.Error(rw, "Error when Checking for Existing Carts", http.StatusBadRequest)
 	}
-	fmt.Println("key", val)
-
-	val2, err := client.Get("key2").Result()
-	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
-	} else if err != nil {
-		panic(err)
+	if cart == "" {
+		client.Set(string(uuid.NewRandom()), id, 0)
 	} else {
-		fmt.Println("key2", val2)
+		client.Append(cart, ","+id)
 	}
-	// Output: key value
-	// key2 does not exist
+
+}
+
+// GetCartItems Get Cart Items
+func GetCartItems(rw http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	InitRedisConnection()
+	cart, err := GetExistingCart()
+	if err != nil {
+		log.Errorln(err)
+		http.Error(rw, "Error when Checking for Existing Carts", http.StatusBadRequest)
+	}
+	cartItems, err := client.Get(cart).Result()
+	if err != nil {
+		log.Errorln(err)
+		http.Error(rw, "Error when retriving Items", http.StatusBadRequest)
+	}
+	items := strings.Split(cartItems, ",")
+	log.Infoln(items)
+	json.NewEncoder(rw).Encode(items)
+}
+
+//GetExistingCart get Existing Cart
+func GetExistingCart() (string, error) {
+	val, err := client.Keys("*").Result()
+	if err != nil {
+		log.Error(err)
+		return "", err
+	}
+
+	if len(val) == 0 {
+		return "", nil
+	}
+
+	return val[0], nil
+}
+
+//InitRedisConnection Initializing a Redis Connection
+func InitRedisConnection() {
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	log.Infoln("Initializing a Redis Connection.")
 }
